ef7ab3f6418e49718f680122983b225e0d698d75
David Peralez <davidperalez89@gmail.com>
2024-04-18 11:23:38 -0700

    added state and employees

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("dev");
+  const showEmployees = true;
   return (
     <div className="App">
-      <p>Hello world!</p>
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="David" role="Intern" />
+          <Employee name="Andrea" role={role} />
+          <Employee name="TARS" />
+          <Employee name="CASE" />
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
