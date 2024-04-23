02cc9ced2c66e217c079ffaacec1445f314f132c
David Peralez <davidperalez89@gmail.com>
2024-04-22 17:15:33 -0700

    added NavLink and active pages

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import { v4 as uuidv4 } from "uuid";
 import EditEmployee from "./components/EditEmployee";
 import Header from "./Header";
 import Employees from "./pages/Employees";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Header>
-      <Employees />
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/employees" element={<Employees />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
