17b2b7e269f58fe14ce68253672d6c0fb175797c
David Peralez <davidperalez89@gmail.com>
2024-04-27 15:15:36 -0700

    status codes

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,32 +1,77 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import NotFound from "../components/NotFound";
 
 export default function Definition() {
   const [word, setWord] = useState();
+  const [notfound, setNotFound] = useState(false);
+
+  const [error, setError] = useState(false);
+
   let { search } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search)
-      .then((response) => response.json())
+    // const url = "http://httpstat.us/501";
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
+    fetch(url)
+      .then((response) => {
+        console.log(response.status);
+        if (response.status === 404) {
+          setNotFound(true);
+        } else if (response.status === 401) {
+          navigate("/login");
+        } else if (response.status === 500) {
+          setError(true);
+        }
+
+        if (!response.ok) {
+          setError(true);
+
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
       .then((data) => {
         setWord(data[0].meanings);
+      })
+      .catch((e) => {
+        console.log(e.message);
       });
   }, []);
 
+  if (notfound === true) {
+    return (
+      <>
+        <NotFound />
+        <Link to="/dictionary">Search another</Link>
+      </>
+    );
+  }
+  if (error === true) {
+    return (
+      <>
+        <p>Something went wrong, try again</p>
+        <Link to="/dictionary">Search another</Link>
+      </>
+    );
+  }
   return (
     <>
-      <h1>Here is a Definition: </h1>
-      {word
-        ? word.map((meaning) => {
+      {word ? (
+        <>
+          <h1>Here is a Definition: </h1>
+          {word.map((meaning) => {
             return (
               <p key={uuidv4()}>
                 {meaning.partOfSpeech + ": "}
                 {meaning.definitions[0].definition}
               </p>
             );
-          })
-        : null}
+          })}
+        </>
+      ) : null}
     </>
   );
 }
